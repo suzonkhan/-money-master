6b10c654b102d551ba2fc964be38cff65b0ddca3
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-02-17 10:37:58 +0600

    Added comment into JS

--- js/main-script.js
+++ js/main-script.js
@@ -1,3 +1,4 @@
+// Get all global element 
 const  finalWarningElement =  document.getElementById("calculation-warning");
 const  expensesElement =  document.getElementById("total-expenses");
 const  primaryBalanceElement =  document.getElementById("primary-balance");
@@ -5,18 +6,23 @@ const  savingElement =  document.getElementById("seving-amount");
 const  netBalanceElement =  document.getElementById("net-balance");
 const  overSavingElement =  document.getElementById("over-save-warning");
 
+// Global data validation for fields 
 function dataValidation(warningElement, warningMsg) {
     const userWarning = document.getElementById(warningElement);
     userWarning.innerText = warningMsg;
-    userWarning.style.display = "block"
+    userWarning.style.display = "block";
 } 
 
+// Recive fields data 
 function getUserData(field) {
     const inputElement = field + "-field";
     const warningElement = field + "-warning"; 
     const userData = parseFloat(document.getElementById(inputElement).value); 
+
+    // Hide error message at another submit
     document.getElementById(warningElement).style.display = "none";
 
+    // Check user input data and return data
     if(isNaN(userData)){
         dataValidation(warningElement, "Please insert number only.")
         return false;
@@ -29,23 +35,32 @@ function getUserData(field) {
 }
 
 document.getElementById("cost-calculate").addEventListener("click", function(){
+    //Receive user data from input fields
     const income = getUserData("income");
     const foodCost = getUserData("food");
     const rentCost = getUserData("rent");
     const clothesCost = getUserData("clothes");
+
+    //Checking all user data again and get single return after validation 
     const dataValidation =  calculationValidation(income, foodCost, rentCost, clothesCost);
+    
+    //Calculate cost 
     const totalCost = foodCost + rentCost + clothesCost;
+
+    // Hide error message at another submit
     finalWarningElement.style.display = "none";
-   
+    
+    // Display Income and total cost into webpage after validation   
      if(dataValidation && income > totalCost){
         expensesElement.innerText = totalCost;
         primaryBalanceElement.innerText = income - totalCost
      } else {
-        finalWarningElement.innerText = "Income shode more then cost.";
+        finalWarningElement.innerText = "Income should be more than expenses.";
         finalWarningElement.style.display = "block";
      }  
 })
 
+//Checking all user data again and get single return for calculation
 function calculationValidation() {
     let validation = true;
     for(const data of arguments){
@@ -57,13 +72,25 @@ function calculationValidation() {
     return validation;
 }
 
+// Saving calculation 
 document.getElementById("saving-calculator").addEventListener("click", function(){
+     //Receive user data from input fields
     const income = getUserData("income");
     const saveing = getUserData("save");
+
+    //Receive balance from previous calculation 
     const primaryBalance = parseFloat(primaryBalanceElement.innerText);
+
+    // Get seving amount from percent 
     const  sevingAmount = (saveing / 100) * income;
+   
+    // Get net balance
     const netBalance = primaryBalance - sevingAmount;
+
+    // Hide error message at another submit
     overSavingElement.style.display = "none";
+
+    // Display Seving and net balance into webpage after validation 
     if( primaryBalance > sevingAmount && saveing > 0){
        savingElement.innerText = sevingAmount;
        netBalanceElement.innerText = netBalance;
